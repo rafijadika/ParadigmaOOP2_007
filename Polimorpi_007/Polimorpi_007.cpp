0cc65824f4fb47f435ba6cde930092b3ea6553a7
rafijadika <jadika.rafi@gmail.com>
2023-05-31 20:38:02 +0700

    commit 23

--- Polimorpi_007/Polimorpi_007.cpp
+++ Polimorpi_007/Polimorpi_007.cpp
@@ -1,20 +1,42 @@
-// Polimorpi_007.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
+
+class seseorang {
+public:
+    virtual void pesan() = 0;
+    // virtual void pesan(){
+    //      cout <<"pesan dari seseorang"<<edl;
+    // }
+};
+
+class joko : public seseorang {
+public:
+    void pesan() {
+        cout << "Pesan dari Joko" << endl;
+
+    }
+};
+
+class lia : public seseorang {
+public:
+    void pesan() {
+        cout << "Pesan dari Lia" << endl;
+    }
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    seseorang* obyek;
+    joko a;
+    lia b;
+
+    obyek = &a;
+    obyek->pesan();
+    obyek = &b;
+    obyek->pesan();
+    // a.seseorang::pesan();
+
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
